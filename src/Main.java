842b7e81f351d03189cfbf6487836557559ce50c
koksyncho <hrokishmokibarbaroki@gmail.com>
2017-01-23 00:32:27 +0200

    Buying and auctioning properties
    Organised some of the code from the last commit into methods and added some not so organised code (for buying and auctioning mainly).

--- src/Main.java
+++ src/Main.java
@@ -2,76 +2,278 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
-
+	private static String board[] = new String[40];
+	private static int playerAmount;
+	private static int overallTurns = 1;
+	private static String[] playerNames = new String[4];
+	private static int[] playerMoney = {2000, 2000, 2000, 2000};
+	private static int[] firstDiceRows = new int[4];
+	private static int[] secondDiceRows = new int[4];
+	private static char confirm;
+	private static String fieldAction;
+	private static String fieldOwner;
+	private static String fieldCost;
+	
+	private static boolean isReady = false;
+	private static boolean hasWon = false;
+	
+	static Scanner input = new Scanner(System.in);
+	static Random rand = new Random();
+	
+	//due to the variables being outside of the main most functions can manage without a return
+	
 	public static void main(String[] args) {
-		String board[] = new String[40];
+
 		fillTheBoardWithTheNeededInformation(board);
-		int playerAmount;
-		String[] playerNames = new String[4];
-		int[] firstDiceRows = new int[4];
-		int[] secondDiceRows = new int[4];
-		char confirm;
-
-		boolean isReady = false;
-		boolean hasWon = false;
-
-		Random rand = new Random();
-		Scanner input = new Scanner(System.in);
-
-		System.out.println("Enter the number of players: ");
-
-		// should be at least 2
-		playerAmount = input.nextInt();
+		inputThePlayerAmount();
 		
 		int[] playerLocation = new int[playerAmount];
+				
+		inputThePlayerNames();
 
+
+		while (!hasWon) {	
+			outputTurn();
+			makeTurn(playerLocation);
+		}
+
+	}
+
+	private static void outputTurn() {
+		System.out.println("TURN " + overallTurns++);
+	}
+
+	private static void makeTurn(int[] playerLocation) {
+		for (int i = 0; i < playerAmount; i++) {
+			System.out.println(playerNames[i] + "'s turn: ");
+			
+			rowTheDices(i);
+			
+			if (!hasMadeAFullLapOfTheField(playerLocation, i)) {
+				playerLocation[i] += firstDiceRows[i] + secondDiceRows[i];
+			} else {
+				playerLocation[i] = (-1) * (40 - playerLocation[i] - firstDiceRows[i] - secondDiceRows[i]);
+			}
+			outputPlayerLocation(playerLocation, i);
+			outputPlayerMoney(i);
+			decideHowToProceedWithTurn(playerLocation, i);
+		}
+
+	}
+
+	private static void outputPlayerMoney(int i) {
+		System.out.println("Money: " + playerMoney[i]);
+	}
+
+	private static void decideHowToProceedWithTurn(int[] playerLocation, int i) {
+		fieldAction = board[playerLocation[i]].split(", ")[2];
+		fieldOwner = board[playerLocation[i]].split(", ")[4];
+		fieldCost = board[playerLocation[i]].split(", ")[3];
+		
+		System.out.println(fieldAction + " " + fieldCost + " " + fieldOwner);
+		if (isOnPurchasableField(/*fieldAction*/)) {
+			if (!isFieldBought()) {
+				do {
+					System.out.println();
+
+					System.out.println("You can (b)uy the field for " + fieldCost + " or start an (a)uction for it.");
+					confirm = input.next().charAt(0);
+					//System.out.println("Testing the -> " + confirm + " <- symbol");
+			
+					if (confirm == 'b'/*doesWantToBuy()*/) {
+						playerMoney[i] -= Integer.parseInt(fieldCost);
+						fieldOwner = playerNames[i];
+						isReady = true;
+					} else if (confirm == 'a'/*doesWantToAuction()*/) {
+						System.out.println("DOING THE AUCTION...");
+						doAuction();
+						isReady = true;
+					} else {
+						System.out.println("Invalid input ((b)uy/(a)uction).");
+						isReady = false;
+					}
+				} while (!isReady);
+			}
+		}
+		
+		System.out.println("Are you ready to proceed or do you want to look at your options? (p)roceed/(o)ptions.");
+
+		isReady = false;	
+		do {
+			System.out.println();
+			
+			confirm = input.next().charAt(0);
+			//System.out.println("Testing the -> " + confirm + " <- symbol");
+	
+			if (doesWantToProceed()) {
+				isReady = true;
+			} else if (doesWantToLookAtTheOptions()) {
+				// should probably be a list of the options
+				isReady = false;
+			} else {
+				outputInvalidInputMessage();
+			}
+		} while (!isReady);
+	
+	}
+
+	private static void doAuction() {
+		//default false
+		boolean[] isPlayerNotInTheAuction = new boolean[playerAmount];
+		int[] bids = new int[playerAmount];
+		int highestBid = 0;
+		char decision;
+		int amountOfParticipatingPlayers = playerAmount; 
+		//int bid;
+		
+		while (amountOfParticipatingPlayers != 1) {
+		
+			for (int i = 0; i < playerAmount; i++) {
+				if(!isPlayerNotInTheAuction[i]) {
+					System.out.println(playerNames[i] + "'s turn: ");
+					if (amountOfParticipatingPlayers == 1) {
+						System.out.println("Being the last player " + playerNames[i] + " won - buying the property for " + highestBid + ".");
+						break;
+					}
+					System.out.println("(b)idding/(l)eaving");
+					decision = input.next().charAt(0);
+					
+					do {
+						if (decision == 'b') {
+							System.out.println("Enter your bid (has to be over " + highestBid + ", you can bail out by bidding below 0): ");
+							do {
+								bids[i] = input.nextInt();
+								//bid = bids[i];
+								if(bids[i] < 0) {
+									isPlayerNotInTheAuction[i] = true;
+									amountOfParticipatingPlayers--;
+								}
+								System.out.println(playerNames[i] + " bid " + bids[i]);
+							} while(bids[i] <= highestBid || isPlayerNotInTheAuction[i]);
+							if(highestBid < bids[i]){
+								highestBid = bids[i];
+							}
+						} else if (decision == 'l') {
+							isPlayerNotInTheAuction[i] = true;
+							amountOfParticipatingPlayers--;
+						} else {
+							//!!doesn't quite work
+							System.out.println("Invalid input ((b)id/(l)eave)..");
+						}
+	//					boolean isB = decision != 'b';
+	//					boolean isL = decision != 'l';
+	//					boolean validDecision = isB || isL;
+	//					System.out.println(isB + " " + isL + " result " + validDecision);
+						//some interesting stuff with the while condition with an ||
+	 				} while(!(decision != 'b' || decision != 'l'));	
+				}	
+			}
+		}
+	}
+
+//	private static void inputWhetherToBuyFieldOrStartAnAuctionForIt() {
+//		System.out.println("Do you buy this field for " + fieldCost + " or try to auction it with the other players? (b)uy/(a)uction");
+//		do {
+//			confirm = input.next().charAt(0);
+//		} while (confirm != 'b' || confirm != 'a');
+//	}
+
+	private static boolean isFieldBought() {
+		boolean isFieldBought = !fieldOwner.equals("no owner");
+		return isFieldBought;
+	}
+
+	private static boolean isOnPurchasableField(/*String fieldAction2*/) {
+		boolean isOnPurchasableField = fieldAction.equals("buy");
+		return isOnPurchasableField;
+	}
+
+	private static void outputInvalidInputMessage() {
+		System.out.println("Invalid input ((p)roceed/(o)ptions).");
+	}
+
+	private static boolean doesWantToProceed() {
+		boolean doesWantToProceed = confirm == 'p';
+		return doesWantToProceed;
+	}
+
+	private static boolean doesWantToLookAtTheOptions() {
+		boolean doesWantToLookAtTheOptions = confirm == 'o';
+		return doesWantToLookAtTheOptions;
+	}
+
+	private static void outputPlayerLocation(int[] playerLocation, int i) {
+		System.out.println("Location: " + playerLocation[i] + " " + board[playerLocation[i]]);
+	}
+
+	private static void rowTheDices(int i) {
+		firstDiceRows[i] = rowDice();
+		outputTheResultOfTheFirstDicesRow(i);
+
+		secondDiceRows[i] = rowDice();
+		outputTheResultOfTheSecondDicesRow(i);
+
+	}
+
+	private static boolean hasMadeAFullLapOfTheField(int[] playerLocation, int i) {
+		boolean hasMadeAFullLap = playerLocation[i] + firstDiceRows[i] + secondDiceRows[i] >= 40;
+		return hasMadeAFullLap;
+	}
+
+	private static void outputTheResultOfTheSecondDicesRow(int index) {
+		System.out.println("Second dice row: " + secondDiceRows[index]);
+	}
+
+	private static void outputTheResultOfTheFirstDicesRow(int index) {
+		System.out.println("First dice row: " + firstDiceRows[index]);
+	}
+
+	private static int rowDice() {
+		int diceRow = rand.nextInt(6) + 1;
+		return diceRow;
+	}
+
+	private static void inputThePlayerNames() {
 		System.out.println("Enter your names: ");
 
 		input.nextLine();
 
-		// nextLine() works in mysterious ways
-
 		for (int i = 0; i < playerAmount; i++) {
 			System.out.println("Name player " + (i + 1) + ": ");
 			playerNames[i] = input.nextLine();
-
 			System.out.println();
 		}
+	}
 
+	private static void inputThePlayerAmount() {		
+		System.out.println("Enter the number of players (minimum 2, maximum 4): ");
+		int countOfPlayerAmountInputAttempts = 0;
+		
 		do {
-			for (int i = 0; i < playerAmount; i++) {
-				System.out.println(playerNames[i] + "'s turn: ");
-				firstDiceRows[i] = rand.nextInt(6) + 1;
-				System.out.println("First dice row: " + firstDiceRows[i]);
+			checkForInvalidInput(countOfPlayerAmountInputAttempts);
+			
+			playerAmount = input.nextInt();
+			
+			countOfPlayerAmountInputAttempts++;
+		} while (isTheNumberOfPlayersItBelowTwoOrOverFour(/*playerAmount*/));
+	}
 
-				secondDiceRows[i] = rand.nextInt(6) + 1;
-				System.out.println("Second dice row: " + secondDiceRows[i]);
+	private static void checkForInvalidInput(int countOfInputAttempts) {
+		if(countOfInputAttempts >= 1) {
+			outputWarningUponInvalidPlayerAmountInput();
+		}
+	}
 
-				if (playerLocation[i] + firstDiceRows[i] + secondDiceRows[i] <= 40) {
-					playerLocation[i] += firstDiceRows[i] + secondDiceRows[i];
-				} else {
-					playerLocation[i] = (-1)
-							* (40 - playerLocation[i] - firstDiceRows[i] - secondDiceRows[i]);
-				}
-				System.out.println("Location: " + playerLocation[i] + " "
-						+ board[playerLocation[i]]);
-				System.out.println("Are you ready to proceed? Y/N.");
+	private static void outputWarningUponInvalidPlayerAmountInput() {
+		System.out.println("The players should be between 2 and 4.");
+	}
 
-				do {
-					System.out.println();
-					confirm = input.next().charAt(0);
-					System.out.println("Testing the -> " + confirm
-							+ " <- symbol");
-					if (confirm == 'Y') {
-						isReady = true;
-					} else if (confirm == 'N' || confirm != 'Y') {
-						// should probably be a list of the options
-						isReady = false;
-					}
-				} while (isReady == false);
-			}
-		} while (hasWon == false);
-
+	private static boolean isTheNumberOfPlayersItBelowTwoOrOverFour(/*int playerAmount*/) {
+		if (playerAmount < 2 || playerAmount > 4) {
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 	private static void fillTheBoardWithTheNeededInformation(String[] board) {
@@ -87,57 +289,7 @@ public class Main {
 		fillPrisons(board);
 		fillParkplace(board);
 		fillTheStart(board);
-
-		// board[0] = "COLLECT $200 AS YOU PASS, go, win, 200, -, -";
-		// board[1] = "COMMUNITY CHEST, chest, draw chest card, -, -, -";
-		// board[2] = "BLAGOEVGRAD ROAD, property, buy, 50, no owner, darkblue";
-		// board[3] = "CARIGRAD ROAD, property, buy, 60, no owner, darkblue";
-		// board[4] = "TAX REWARD, reward, win, 200, -, -";
-		// board[5] = "SOFIA STATION, station, buy, 200, -, -";
-		// board[6] = "LUCKY DRAW, luck, draw luck card, -, -, -";
-		// board[7] = "CHERNI MOUNT, property, buy, 100, no owner, brown";
-		// board[8] = "LOMSKI ROAD, property, buy, 100, no owner, brown";
-		// board[9] = "LUVOV BRIDGE, property, buy, 100, no owner, brown";
-		// board[10] = "PRISON, prison, prison options, -, -, -";
-		// board[11] =
-		// "MAKEDONSKI SQUARE, property, buy, 150, no owner, lightblue";
-		// board[12] =
-		// "PIROTSKA STREET, property, buy, 150, no owner, lightblue";
-		// board[13] = "HRISTO BOTEV, property, buy, 180, no owner, lightblue";
-		// board[14] = "CHEZ, factory, buy, 150, no owner, -";
-		// board[15] = "PLOVDIV STATION, station, buy, 200, -, -";
-		// board[16] = "COMMUNITY CHEST, chest, draw chest card, -, -, -";
-		// board[17] = "EVLOGI GEORGIEV, property, buy, 200, no owner, green";
-		// board[18] = "ORLOV BRIDGE, property, buy, 200, no owner, green";
-		// board[19] = "BULGARIA BULEVARD, property, buy, 220, no owner, green";
-		// board[20] = "PARKING, parking, pay, 0, -, -";
-		// board[21] =
-		// "SAN STEFANO STREET, property, buy, 220, no owner, yellow";
-		// board[22] = "SHIPKA STREET, property, buy, 220, no owner, yellow";
-		// board[23] = "OBORISHTE STREET, property, buy, 240, no owner, yellow";
-		// board[24] = "LUCKY DRAW, luck, draw luck card, -, -, -";
-		// board[25] = "VARNA STATION, station, buy, 200, -, -";
-		// board[26] = "VIK, factory, buy, 150, no owner, -";
-		// board[27] =
-		// "LONDUKOV BULEVARD, property, buy, 280, no owner, purple";
-		// board[28] =
-		// "PATRIARH EVTIMII BULEVARD, property, buy, 280, no owner, purple";
-		// board[29] =
-		// "VASIL LEVSKI BULEVARD, property, buy, 300, no owner, purple";
-		// board[30] = "GO TO JAIL, inprisonment, inprisonment, -, -, -";
-		// board[31] =
-		// "G. S. RAKOVSKI STREET, property, buy, 300, no owner, orange";
-		// board[32] =
-		// "GRAF IGNATIEV STREET, property, buy, 300, no owner, orange";
-		// board[33] =
-		// "G. M. DIMITROV BULEVARD, property, buy, 320, no owner, orange";
-		// board[34] = "COMMUNITY CHEST, chest, draw chest card, -, -, -";
-		// board[35] = "BURGAS STATION, station, buy, 200, -, -";
-		// board[36] = "LUCKY DRAW, luck, draw luck card, -, -, -";
-		// board[37] = "BULEVARD VITOSHA, property, buy, 360, no owner, red";
-		// board[38] = "BOIANA, property, buy, 400, no owner, red";
-		// board[39] = "SUPER TAX, tax, pay, 100, -, -";
-	}
+}
 
 	private static void fillParkplace(String[] board) {
 		board[20] = "PARKING, parking, pay, 0, -, -";
@@ -175,42 +327,66 @@ public class Main {
 	}
 
 	private static void fillTheStations(String[] board) {
-		board[5] = "SOFIA STATION, station, buy, 200, -, -";
-		board[15] = "PLOVDIV STATION, station, buy, 200, -, -";
-		board[25] = "VARNA STATION, station, buy, 200, -, -";
-		board[35] = "BURGAS STATION, station, buy, 200, -, -";
+		board[5] = "SOFIA STATION, station, buy, 200, no owner, -";
+		board[15] = "PLOVDIV STATION, station, buy, 200, no owner, -";
+		board[25] = "VARNA STATION, station, buy, 200, no owner, -";
+		board[35] = "BURGAS STATION, station, buy, 200, no owner, -";
 	}
 
 	private static void fillTheProperties(String[] board) {
-		board[2] = "BLAGOEVGRAD ROAD, property, buy, 50, no owner, darkblue";
-		board[3] = "CARIGRAD ROAD, property, buy, 60, no owner, darkblue";
+		fillTheDarkBlueProperties(board);
+		fillTheBrownProperties(board);
+		fillTheLightBlueProperties(board);
+		fillTheGreenProperties(board);
+		fillTheYellowProperties(board);
+		fillThePurpleProperties(board);
+		fillTheOrangeProperties(board);
+		fillTheRedProperties(board);
+	}
 
-		board[7] = "CHERNI MOUNT, property, buy, 100, no owner, brown";
-		board[8] = "LOMSKI ROAD, property, buy, 100, no owner, brown";
-		board[9] = "LUVOV BRIDGE, property, buy, 100, no owner, brown";
+	private static void fillTheRedProperties(String[] board) {
+		board[37] = "BULEVARD VITOSHA, property, buy, 360, no owner, red";
+		board[38] = "BOIANA, property, buy, 400, no owner, red";
+	}
 
-		board[11] = "MAKEDONSKI SQUARE, property, buy, 150, no owner, lightblue";
-		board[12] = "PIROTSKA STREET, property, buy, 150, no owner, lightblue";
-		board[13] = "HRISTO BOTEV, property, buy, 180, no owner, lightblue";
-
-		board[17] = "EVLOGI GEORGIEV, property, buy, 200, no owner, green";
-		board[18] = "ORLOV BRIDGE, property, buy, 200, no owner, green";
-		board[19] = "BULGARIA BULEVARD, property, buy, 220, no owner, green";
-
-		board[21] = "SAN STEFANO STREET, property, buy, 220, no owner, yellow";
-		board[22] = "SHIPKA STREET, property, buy, 220, no owner, yellow";
-		board[23] = "OBORISHTE STREET, property, buy, 240, no owner, yellow";
-
-		board[27] = "LONDUKOV BULEVARD, property, buy, 280, no owner, purple";
-		board[28] = "PATRIARH EVTIMII BULEVARD, property, buy, 280, no owner, purple";
-		board[29] = "VASIL LEVSKI BULEVARD, property, buy, 300, no owner, purple";
-
+	private static void fillTheOrangeProperties(String[] board) {
 		board[31] = "G. S. RAKOVSKI STREET, property, buy, 300, no owner, orange";
 		board[32] = "GRAF IGNATIEV STREET, property, buy, 300, no owner, orange";
 		board[33] = "G. M. DIMITROV BULEVARD, property, buy, 320, no owner, orange";
+	}
 
-		board[37] = "BULEVARD VITOSHA, property, buy, 360, no owner, red";
-		board[38] = "BOIANA, property, buy, 400, no owner, red";
+	private static void fillThePurpleProperties(String[] board) {
+		board[27] = "LONDUKOV BULEVARD, property, buy, 280, no owner, purple";
+		board[28] = "PATRIARH EVTIMII BULEVARD, property, buy, 280, no owner, purple";
+		board[29] = "VASIL LEVSKI BULEVARD, property, buy, 300, no owner, purple";
+	}
 
+	private static void fillTheYellowProperties(String[] board) {
+		board[21] = "SAN STEFANO STREET, property, buy, 220, no owner, yellow";
+		board[22] = "SHIPKA STREET, property, buy, 220, no owner, yellow";
+		board[23] = "OBORISHTE STREET, property, buy, 240, no owner, yellow";
+	}
+
+	private static void fillTheGreenProperties(String[] board) {
+		board[17] = "EVLOGI GEORGIEV, property, buy, 200, no owner, green";
+		board[18] = "ORLOV BRIDGE, property, buy, 200, no owner, green";
+		board[19] = "BULGARIA BULEVARD, property, buy, 220, no owner, green";
+	}
+
+	private static void fillTheLightBlueProperties(String[] board) {
+		board[11] = "MAKEDONSKI SQUARE, property, buy, 150, no owner, lightblue";
+		board[12] = "PIROTSKA STREET, property, buy, 150, no owner, lightblue";
+		board[13] = "HRISTO BOTEV, property, buy, 180, no owner, lightblue";
+	}
+
+	private static void fillTheBrownProperties(String[] board) {
+		board[7] = "CHERNI MOUNT, property, buy, 100, no owner, brown";
+		board[8] = "LOMSKI ROAD, property, buy, 100, no owner, brown";
+		board[9] = "LUVOV BRIDGE, property, buy, 100, no owner, brown";
+	}
+
+	private static void fillTheDarkBlueProperties(String[] board) {
+		board[2] = "BLAGOEVGRAD ROAD, property, buy, 50, no owner, darkblue";
+		board[3] = "CARIGRAD ROAD, property, buy, 60, no owner, darkblue";
 	}
 }
